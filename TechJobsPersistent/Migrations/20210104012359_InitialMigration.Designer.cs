c2086a62143b4191aa6ec02f299a30182056a5c4
DeJohn Nelson <dejohn.nelson1998@gmail.com>
2021-01-03 19:27:17 -0600

    Continued..

--- TechJobsPersistent/Migrations/20210104012359_InitialMigration.Designer.cs
+++ TechJobsPersistent/Migrations/20210104012359_InitialMigration.Designer.cs
@@ -8,7 +8,7 @@ using TechJobsPersistent.Data;
 namespace TechJobsPersistent.Migrations
 {
     [DbContext(typeof(JobDbContext))]
-    [Migration("20210104004027_InitialMigration")]
+    [Migration("20210104012359_InitialMigration")]
     partial class InitialMigration
     {
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
